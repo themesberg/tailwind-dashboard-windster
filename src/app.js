65327d08ba08789ad210385d8c8647bdd53a1531
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2021-11-16 17:42:25 +0200

    Update meta description.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import './style.css';
-import 'svgmap/dist/svgMap.min.css';
 import '@themesberg/flowbite';
 import './sidebar';
 import './charts';
-// import './calendar';
 
 // Have the courage to follow your heart and intuition.
